25d5187428c76d644065061a0eb5c0494151ca1d
Arman Roohi <armaanroohi@gmail.com>
2025-05-27 11:10:01 +1000

    rds connect

--- config.php
+++ config.php
@@ -1,3 +1,4 @@
+<?php
 $host = getenv('db_host');
 $user = getenv('db_user');
 $password = getenv('db_pass');
@@ -8,3 +9,4 @@ $conn = mysqli_connect($host, $user, $password, $dbname);
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+?>
